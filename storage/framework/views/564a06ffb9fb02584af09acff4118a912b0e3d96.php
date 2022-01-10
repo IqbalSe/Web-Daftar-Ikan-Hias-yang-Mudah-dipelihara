3bdbd0ff23b4bff4027892d1bf828e832aa9da23
Iqbal Setiawan <93498738+IqbalSe@users.noreply.github.com>
2022-01-10 08:52:31 +0700

    Add files via upload

---
+++ storage/framework/views/564a06ffb9fb02584af09acff4118a912b0e3d96.php
@@ -0,0 +1,87 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://2.bp.blogspot.com/-9XadjCAIkLM/W2vCXzi5d0I/AAAAAAAAHRk/2MrV2fmUIggFl-oL8homHZ5HnXO9cl__ACLcBGAs/s320/ikan-koki-oranda.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Koki Oranda</h5>
+              <p class="card-text"> Sekilas terlihat seperti jenis lionhead yang memiliki gumpalan merah pada bagian kepala. Namun jika dilihat lebih dekat, hewan hias ini memiliki ekor yang lebih panjang dan warna putih pekat yang mendominasi. Karena faktor inilah akhirnya ikan yang bisa tumbuh dari 5- 10 cm ini, memiliki banyak penggemar.</p>
+            
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://www.dictio.id/uploads/db3342/original/3X/c/7/c75b6479057bae0444aba3f91faba0e28a73573a.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Koki Ryukin</h5>
+              <p class="card-text"> Ikan mas koki ryukin berasal dari Jepang dan terkenal karena bentuknya yang lucu seperti Koi. Memiliki tubuh yang pendek, dengan pundak serta sirip yang lebar. Masuk dalam golongan ikan langka dan memiliki nama lain Fringetail, Ribbontail Jepang, Veiltail, dan Fantail.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://ahlihewan.com/wp-content/uploads/2021/07/Ikan-Mas-Mutiara.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Koki Mutiara</h5>
+              <p class="card-text"> Ikan koki mutiara mempunyai keunikan tersendiri dibandingkan dengan jenis ikan koki lainnya. Sehingga, jenis ikan koki mutiara ini sangat mudah dikenali meskipun kita jarang sekali melihat ikan ini dan kita belum mengetahui secara detail ciri-cirinya Bagian yang menjadi ciri khas ikan ini sehingga banyak dikenali oleh banyak orang adalah sisiknya yang timbul dan membentuk seperti mutiara. Oleh karena itu, jenis ikan ini dinamakan ikan koki mutiara atau ikan mas koki mutiara. Selain itu, ikan koki mutiara memiliki perut yang besar dan bulat seperti bola. Ukuran mulut dan kepalanya kecil, ukuran ekornya pendek namun sedikit melebar. Hal ini sangat kontras sekali dengan ukuran badannya atau perutnya.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://www.99.co/blog/indonesia/wp-content/uploads/2021/08/Butterfly-Tail.png">
+            <div class="card-body">
+              <h5 class="card-title">Koki Butterfly tail</h5>
+              <p class="card-text">Ikan Koki Butterfly Tail memiliki ekor yang sangat cantik layaknya sayap kupu-kupu. Sehingga tidak mengherankan jika memiliki nama lain Butterfly Telescope. Jenis ini merupakan salah satu jenis ikan terlangka di dunia, dan bisa mencapai panjang tubuh hingga 5 – 7 cm.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://cdnwpedutorenews.gramedia.net/wp-content/uploads/2021/06/17112352/Ikan-Mas-Koki-Black-Moor-.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Koki Black Moor</h5>
+              <p class="card-text"> Ikan mas atau gold fish biasa dikenal memiliki warna-warna yang cerah seperti orange atau merah dengan sedikit warna putih. Namun jenis ikan mas koki satu ini berbeda. Sesuai dengan namanya, Black Moor merupakan ikan mas koki yang berwarna hitam. Namun warna hitamnya ini menjadi salah satu ciri khas ikan mas koki Black Moor, selain itu ikan ini dikenali memiliki mata yang menonjol ke luar.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://www.bebeja.com/wp-content/uploads/2014/02/maskoki-ranchu.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Koki Ranchu</h5>
+              <p class="card-text"> Ikan mas koki memiliki ciri khas pada punggungnya yang sedikit bungkuk. Ikan mas koki Ranchu juga tidak memiliki sirip pada bagian atas badannya. Kepala ikan mas koki Ranchu ini terlihat sama seperti Oranda serta Lion Head, karena memiliki gumpalan kecil disekitar kepala. Ikan mas koki Ranchu umumnya memiliki warna-warna yang mencolok serta terang dengan bentuk ekor pendek.</p>
+</div>            
+</div>
+</div>
+</div>
+</div>           
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\web-daftar-ikan-hias-yang-mudah-dipelihara\resources\views/IkanKoki.blade.php ENDPATH**/ ?>
