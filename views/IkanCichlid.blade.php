3803eabbf9ce2206691c88f3d4e75d41250c7f4e
Iqbal Setiawan <93498738+IqbalSe@users.noreply.github.com>
2022-01-10 08:51:23 +0700

    Add files via upload

---
+++ views/IkanCichlid.blade.php
@@ -0,0 +1,86 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://aquapedia.id/wp-content/uploads/2020/05/Trewavas-Red-Finned-Cichlid.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Trewavas Red-Finned Cichlid</h5>
+              <p class="card-text"> Ukuran ikan cichlid yang satu ini yaitu sekitar 38,1 cm. Warna pada tubuhnya yaitu biru tua mengkilap dengan sirip yang berwarna merah. Nama ilmiah/latinnya yaitu Labeotropheus trewavasae.</p>
+            
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://aquapedia.id/wp-content/uploads/2020/05/Dogtooth-Cichlid.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Dogtooth Cichlid</h5>
+              <p class="card-text"> Ukuran ikan cichlid jenis dogtooth ini tidak terlalu panjang, yaitu sekitar 10 cm saja. Warna tubuhnya yaitu biru muda garis hitam. Nama ilmiahnya adalah Cynotilapia afra.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://aquapedia.id/wp-content/uploads/2020/05/Redfin-Kadango.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Red Fin Kadango</h5>
+              <p class="card-text"> Ikan dengan nama ilmiah Copadichromis borleyi ini panjang tubuhnya yaitu sekitar 17 cm. Warna pada bagian tubuhnya adalah merah, sesuai dengan namanya. Namun ada sedikit warna biru mengkilap di bagian kepala dan siripnya.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://aquapedia.id/wp-content/uploads/2020/05/Ikan-lemon.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Electric Yellow Cichlid</h5>
+              <p class="card-text">Jenis ini juga sangat populer dengan nama Ikan Lemon. Sesuai juga dengan namanya, cichlid yang satu ini memiliki tubuh berwarna kuning mencolok tetapi bagian siripnya berwarna hitam. Ukurannya pun tidak panjang, hanya sekitar 8,1 cm saja.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://aquapedia.id/wp-content/uploads/2020/05/maingano-cichlid.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Maingano</h5>
+              <p class="card-text"> Campuran antara biru tua mengkilap dengan putih adalah warna dari tubuh cichlid jenis Maingano. Ukuran tubuhnya hanya sekitar 7,5 cm, yang artinya lebih pendek dibanding jenis cichlid yang lainnya.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://aquapedia.id/wp-content/uploads/2020/05/electric-blue-hap-cichlid.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Electric Blue Hap</h5>
+              <p class="card-text"> Nama ilmiah dari cichlid berwarna biru menyala ini adalah Sciaenochromis fryeri. Ikan cichlid ini disebut juga dengan jenis Hap Ahli. Ukuran tubuhnya sekitar 15,5 cm.</p>
+</div>            
+</div>
+</div>
+</div>
+</div>           
+@endsection
---
+++ views/index.blade.php
@@ -0,0 +1,86 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://id-test-11.slatic.net/p/c3e207afeb4e5042d3a57e5ed04572b1.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Channa Maruliodes</h5>
+              <p class="card-text"> Dengan tampilan cukup sangar, Channa Marulioides memiliki warna dasar putih kekuningan dengan pola garis vertikal berwarna hitam mulai dari kepala hingga ekor. Warna biru terang di bagian siripnya juga menambah keunikan ikan ini.Channa Marulioides atau yang biasa disebut emperor snakehead dapat tumbuh hingga 90 cm dan bisa disimpan dalam akuarium.</p>
+            
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://progresnews.id/wp-content/uploads/2020/11/aa2c697b-91bb-4e72-a3dd-d5cdf0519bbb.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Channa Blue Pulcra</h5>
+              <p class="card-text"> Berbeda dengan ikan channa maru yang merupakan ikan channa asli indonesia, ikan channa pulchra ini berasal dari perairan Myanmar bagian barat. Ikan ini hanya dapat tumbuh maksimal kurang lebih 30 cm, seperti ikan channa yang lain channa pulchra dapat hidup pada kondisi perairan yang rendah kadar oksigennya berkat kemampuannya yang dapat mengambil oksigen langsung di udara.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://1.bp.blogspot.com/-KmEChWO2fnk/YQqZOLmXu3I/AAAAAAAAHxU/zxrGokz8yOcP22T8EcxvxswE4SvRe7E9QCLcBGAsYHQ/s800/3ab73f278e0b44e7a50d882f03800448-01.jpeg">
+            <div class="card-body">
+              <h5 class="card-title">Channa Andrao</h5>
+              <p class="card-text"> Ikan Channa andrao adalah spesies yang dijelaskan pada 2013 dari West Bengal, India. Spesies Channa andrao dibedakan dari semua kerabatnya. Channa andrao meningkatkan jumlah spesies ikan ular endemik Himalaya timur menjadi sepuluh, yang hampir sepertiga dari spesies yang diketahui dalam genus.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://i0.wp.com/nakamaaquatics.id/wp-content/uploads/2021/02/channa-bleheri-fisik.png?resize=1024%2C539&ssl=1">
+            <div class="card-body">
+              <h5 class="card-title">Channa Bleheri</h5>
+              <p class="card-text">Channa Bleheri bisa menjadi pilihan untuk kamu yang mencari ikan dengan tingkah laku damai dan dapat bergabung dengan ikan lainnya. Ukuran ikan ini cukup mungil dengan panjang tubuh 17-23 cm. Channa Bleheri menjadi salah satu ikan gabus terfavorit karena warnanya cukup cantik menyerupai pelangi dengan sirip berwarna oranye.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://1.bp.blogspot.com/-xJRo9vI8eKc/XdqKkebTbtI/AAAAAAAAJFs/fB1ouV_QLzYpngG26-srC5StBQ4ntqCqQCLcBGAsYHQ/s1600/Channa-aurantimaculata-harga-ikanesia.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Channa Auranti</h5>
+              <p class="card-text"> Ikan Channa Auranti memiliki ukuran 40 cm. Selain itu, ikan ini memiliki tubuh berwarna coklat dengan campuran garis-garis oranye vertikal. Bentuk kepala Channa Auranti lebih dengan sirip besar yang membentang.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://pbs.twimg.com/media/B6qKN4IIYAANZOc.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Channa Barca</h5>
+              <p class="card-text"> Banyak orang mengira bahwa ikan Channa Barca berasal dari Barcelona, Spanyol. Nyatanya, ikan ini merupakan spesies asli lembah sungai Brahmaputra bagian atas di timur laut India dan Bangladesh yang dapat tumbuh hingga 90 cm. Saat ini, Channa Barca merupakan ikan gabus termahal di Indonesia, Toppers. Harganya bisa mencapai puluhan juta dengan hitungan 1 juta untuk setiap sentimeter. Rata-rata panjang ikan ini adalah 70-80 cm.</p>
+</div>            
+</div>
+</div>
+</div>
+</div>           
+@endsection
